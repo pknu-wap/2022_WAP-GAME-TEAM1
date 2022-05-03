a4a65630237682e8f1980aeb903d75e970bf73e8
심재욱 <vycfum@naver.com>
2022-05-03 22:44:23 +0900

    PlayerLogic

---
+++ Unity/Assets/Scripts/Spike.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.gameObject.SetActive(false);
+        }
+    }
+}
