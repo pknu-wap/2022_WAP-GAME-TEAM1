3d1e6fb83edff02141bc1a376e338338d2f4b978
Munbin-Lee <3412mb@gmail.com>
2022-06-06 16:12:08 +0900

    material 변경, 맵 수정

--- Unity/Assets/Scripts/Player/PlayerController.cs
+++ Unity/Assets/Scripts/Player/PlayerController.cs
@@ -23,10 +23,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     // groundLayer 판단
     private LayerMask groundLayer;
-    [SerializeField]
-    private LayerMask MovingBlockLayer;
     // 캡슐 콜라이더 
-    private CapsuleCollider2D capsuleCollider2D;
+    private BoxCollider2D boxCollider2D;
     //발이 땅에 닿아있는지 여부 판단
     private bool isGrounded;
     //발의 포지션 
@@ -44,7 +42,7 @@ public class PlayerController : MonoBehaviour
         if (instance == null)
         {
             theRB = GetComponent<Rigidbody2D>();
-            capsuleCollider2D = GetComponent<CapsuleCollider2D>();
+            boxCollider2D = GetComponent<BoxCollider2D>();
             anim = GetComponent<Animator>();
             instance = this;
             DontDestroyOnLoad(this.gameObject);
@@ -63,8 +61,7 @@ public class PlayerController : MonoBehaviour
     {
         if (CanMove)
         {
-            theRB.velocity = new Vector2(moveSpeed * Input.GetAxisRaw("Horizontal"), theRB.velocity.y)
-                            + blockSpeed;
+            theRB.velocity = new Vector2(moveSpeed * Input.GetAxisRaw("Horizontal"), theRB.velocity.y);
 
             //플레이어의 좌우반전을 y축 회전을 이용하여 구현
             //총알 발사시에 rotation에 맞는 방향으로 발사하기 위해서
@@ -75,20 +72,19 @@ public class PlayerController : MonoBehaviour
                 transform.eulerAngles = new Vector3(0, 180, 0);
 
 
-            // capsuleCollider의 min, max, center등의 위치 정보를 나타냄
-            Bounds bounds = capsuleCollider2D.bounds;
+            Bounds bounds = boxCollider2D.bounds;
 
             //발의 포지션은 콜라이더 범위의 x축 중간, y축 최소 값.
             footPosition = new Vector2(bounds.center.x, bounds.min.y);
 
             // footPosition의 지름 0.1범위 가상의 원 범위를 설정해서 이 범위가 groundLayer에 닿아있으면 true를 반환
-            isGrounded = Physics2D.OverlapCircle(footPosition, 0.1f, groundLayer)
-                        || Physics2D.OverlapCircle(footPosition, 0.1f, MovingBlockLayer);
+            isGrounded = Physics2D.OverlapCircle(footPosition, 0.1f, groundLayer);
 
-            //땅에 닿아있으면 2단 점프 가능 여부 초기화
+            //땅에 닿아있으면 2단 점프 가능 & 무빙블럭 따라 이동
             if (isGrounded)
             {
                 canDoubleJump = true;
+                theRB.velocity += blockSpeed;
             }
 
             // 점프 관련 (Space로 점프)
@@ -104,7 +100,7 @@ public class PlayerController : MonoBehaviour
                     SoundManager.instance.PlaySFX(0);
 
                 }
-                // canDoubleJump가 true면 공중에서 점프 한번 더 가능.
+                // canDoubleJump가 true면 공중에서 점프 한번 더 가능
                 else if (canDoubleJump)
                 {
                     theRB.velocity = new Vector2(theRB.velocity.x, jumpForce * 0.8f);
